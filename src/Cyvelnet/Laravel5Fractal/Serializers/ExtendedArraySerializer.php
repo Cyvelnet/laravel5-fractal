1fee3499b3ce828544a5d2d3f796c58d6492bf03
Cyvelnet <Cyvelnet@gmail.com>
2015-04-24 14:20:23 +0000

    add ExtendedArraySerializer

---
+++ src/Cyvelnet/Laravel5Fractal/Serializers/ExtendedArraySerializer.php
@@ -0,0 +1,69 @@
+<?php namespace Cyvelnet\Laravel5Fractal\Serializers;
+
+use League\Fractal\Pagination\PaginatorInterface;
+use League\Fractal\Serializer\ArraySerializer;
+
+/**
+ * Class ExtendedArraySerializer
+ * @package Cyvelnet\Laravel5Fractal\Serializers
+ */
+class ExtendedArraySerializer extends ArraySerializer
+{
+
+    /**
+     * Serialize the paginator.
+     *
+     * @param PaginatorInterface $paginator
+     *
+     * @return array
+     */
+    public function paginator(PaginatorInterface $paginator)
+    {
+        $currentPage = (int)$paginator->getCurrentPage();
+        $lastPage = (int)$paginator->getLastPage();
+
+        $pagination = array(
+            'total' => (int)$paginator->getTotal(),
+            'count' => (int)$paginator->getCount(),
+            'per_page' => (int)$paginator->getPerPage(),
+            'current_page' => $currentPage,
+            'total_pages' => $lastPage,
+        );
+
+        $pagination['links'] = array();
+
+        if($currentPage !== 1)
+        {
+            $pagination['links']['first'] = $paginator->getUrl(1);
+        }
+
+        if ($currentPage > 1) {
+            $pagination['links']['previous'] = $paginator->getUrl($currentPage - 1);
+        }
+
+        // add fast backward
+        if(($fastBack = ($currentPage - 10)) >= 1)
+        {
+            $pagination['links']['fastback'] = $paginator->getUrl($fastBack);
+        }
+
+        if ($currentPage < $lastPage) {
+            $pagination['links']['next'] = $paginator->getUrl($currentPage + 1);
+        }
+
+        // add fast backward
+        if(($fastForward = ($currentPage + 10)) <= $lastPage)
+        {
+            $pagination['links']['fastforward'] = $paginator->getUrl($fastForward);
+        }
+
+
+        if ($lastPage > 1 && $currentPage !== $lastPage) {
+
+            $pagination['links']['last'] = $paginator->getUrl($lastPage);
+        }
+
+
+        return array('pagination' => $pagination);
+    }
+}
