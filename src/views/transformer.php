9e6a4845e1a6ff301a5ab7cf4e8525827efe71e4
cyvelnet <cyvelnet@gmail.com>
2015-12-22 17:12:41 +0000

    uses pure php in favor of blade.php for transformer stub

---
+++ src/views/transformer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Transformers;
+
+use League\Fractal;
+use League\Fractal\TransformerAbstract;
+use League\Fractal\Resource\Collection;
+use League\Fractal\Resource\Item;
+
+class UserTransformer extends TransformerAbstract
+{
+    /**
+     * List of resources possible to include
+     *
+     * @var array
+     */
+    protected $availableIncludes = [];
+
+    /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected $defaultIncludes = [];
+
+    /**
+     * Transform object into a generic array
+     *
+     * @var object
+     */
+    public function transform($resource)
+    {
+        //return new \League\Fractal\Resource\Collection($resource,new ResourceTransformer);
+    }
+
+}
