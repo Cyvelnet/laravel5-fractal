c87d6461cc9321cf97b3e06c13a6551185a83809
cyvelnet <cyvelnet@gmail.com>
2016-09-13 18:32:06 +0800

    fixes model not found on empty model option

--- src/Cyvelnet/Laravel5Fractal/Commands/TransformerGeneratorCommand.php
+++ src/Cyvelnet/Laravel5Fractal/Commands/TransformerGeneratorCommand.php
@@ -221,38 +221,43 @@ class TransformerGeneratorCommand extends Command
 
         $classNamespace = "\\{$namespace}\\{$class}";
 
-        if (class_exists($classNamespace)) {
-            $model = new \ReflectionClass($classNamespace);
+       if ($class)
+       {
 
-            if ($model->isSubclassOf('Illuminate\Database\Eloquent\Model')) {
-                $mdl = $this->app->make($classNamespace);
-                $table = $mdl->getConnection()->getTablePrefix().$mdl->getTable();
-                $schema = $mdl->getConnection()->getDoctrineSchemaManager($table);
+           if (class_exists($classNamespace)) {
+               $model = new \ReflectionClass($classNamespace);
 
-                $database = null;
-                if (strpos($table, '.')) {
-                    list($database, $table) = explode('.', $table);
-                }
+               if ($model->isSubclassOf('Illuminate\Database\Eloquent\Model')) {
+                   $mdl = $this->app->make($classNamespace);
+                   $table = $mdl->getConnection()->getTablePrefix().$mdl->getTable();
+                   $schema = $mdl->getConnection()->getDoctrineSchemaManager($table);
 
-                $columns = Arr::except($schema->listTableColumns($table, $database), $mdl->getHidden());
+                   $database = null;
+                   if (strpos($table, '.')) {
+                       list($database, $table) = explode('.', $table);
+                   }
+
+                   $columns = Arr::except($schema->listTableColumns($table, $database), $mdl->getHidden());
 
 
-                foreach ($columns as $column) {
-                    if ($column->getType() instanceof \Doctrine\DBAL\Types\JsonArrayType) {
-                        continue;
-                    }
+                   foreach ($columns as $column) {
+                       if ($column->getType() instanceof \Doctrine\DBAL\Types\JsonArrayType) {
+                           continue;
+                       }
 
-                    $castTo = $this->getCasting($column->getType());
+                       $castTo = $this->getCasting($column->getType());
 
-                    $attributes[] = ['column' => $column->getName(), 'casts' => $castTo];
-                }
+                       $attributes[] = ['column' => $column->getName(), 'casts' => $castTo];
+                   }
 
-                return $attributes;
-            }
-        } else {
-            $this->error("Your model {$class} was not found in {$namespace}\\ \r\nIf this is the first time you get this message, try to update /config/fractal.php to make changes to model_namespace accordingly.");
-            exit();
-        }
+                   return $attributes;
+               }
+           } else {
+               $this->error("Your model {$class} was not found in {$namespace}\\ \r\nIf this is the first time you get this message, try to update /config/fractal.php to make changes to model_namespace accordingly.");
+               exit();
+           }
+
+       }
 
         // use as a default attribute
         return [
